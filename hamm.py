d7dacda77fe7fe20d1577d6346abea7de3b60c96
lindsey <hello@lrallison.com>
2015-09-25 19:06:05 -0500

    add hamm

---
+++ hamm.py
@@ -0,0 +1,13 @@
+# Python has a distance package with a built in calculator. 
+# https://pypi.python.org/pypi/Distance/0.1
+# Below is a proof of concept
+
+s= GAGCCTACTAACGGGAT
+t= CATCGTAATGACGGCCT
+
+def hamm(s1, s2):
+	#check to see if they're the same length
+	assert len(s1) == len(s2)
+	return
+
+
