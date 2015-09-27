18ea3b777a624b66f14140783610a6729673f8f3
lindsey <hello@lrallison.com>
2015-09-27 13:56:34 -0500

    Adding hamm

--- hamm.py
+++ hamm.py
@@ -1,13 +1,24 @@
 # Python has a distance package with a built in calculator. 
 # https://pypi.python.org/pypi/Distance/0.1
+# NumPy can do it easily as well
 # Below is a proof of concept
 
-s= GAGCCTACTAACGGGAT
-t= CATCGTAATGACGGCCT
+##Problem
 
-def hamm(s1, s2):
+#Given two strings s and t of equal length, the Hamming distance between s and t, denoted dH(s,t), is the number of corresponding symbols that differ in s and t. See Figure 2.
+
+#Given: Two DNA strings s and t of equal length (not exceeding 1 kbp).
+
+#Return: The Hamming distance dH(s,t).
+
+s= 'GAGCCTACTAACGGGAT'
+t= 'CATCGTAATGACGGCCT'
+
+def hamm(s, t):
 	#check to see if they're the same length
-	assert len(s1) == len(s2)
-	return
+	assert len(s) == len(t)
+	return sum(ch1 != ch2 for ch1, ch2 in zip(s, t))
+
+print hamm(s,t)
 
 
