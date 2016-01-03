8213f5e5d336dd8e834cb627404403c40a6903f6
lindsey <hello@lrallison.com>
2016-01-02 21:21:46 -0600

    fasta mods

--- hmotif.py
+++ hmotif.py
@@ -1,20 +1,22 @@
-##groupby is loading everything as a tuple not a list. The conversion is slow, and I'm trying to figure out how to dix it. I'm about to use biopython at least for fasta parsing. 
+fasta = open("rosalind_lcsm.fasta", "r")
 
-
-from itertools import groupby
-
-with open('rosalind_lcsm.txt') as r:
-    strint = groupby(x.strip() for x in r.readlines() if not x.startswith('>'))
-
+def parseFasta(fasta):
+     sequence = ""
+     for line in fasta:
+          if ">" in line:
+               pass
+          else:
+               sequence += line.strip()
+     return sequence.split()
 
 def hid_motif(data):
   substrs = lambda x: {x[i:i+j] for i in range(len(x)) for j in range(len(x) - i + 1)}
-  s = substrs(data[0])
+  s = (substrs(data[0]))
   for val in data[1:]:
     s.intersection_update(substrs(val))
   return max(s, key=len)
 
-print hid_motif(dnahmotif)
+print hid_motif(parseFasta(fasta))
 
 
 
